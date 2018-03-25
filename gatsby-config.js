84be6a1fd7b9af258096e4ed9e4b7c93edb5ea62
Philip Boardman <dustykeyboard@brd.mn>
2018-03-26 10:19:00 +1100

    Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+  ],
+}
