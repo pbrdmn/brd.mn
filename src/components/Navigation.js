0ea38e7dd0872eae7aa93f393e701a6edbae469b
Philip Boardman <dustykeyboard@brd.mn>
2018-06-21 21:01:46 +1000

    Add GitHub link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 const Navigation = () => (
   <div className="header__links">
     <Link to="/articles">Articles</Link>
+    <a href="https://github.com/dustykeyboard">GitHub</a>
     <a href="https://twitter.com/pbrdmn">@pbrdmn</a>
   </div>
 )
