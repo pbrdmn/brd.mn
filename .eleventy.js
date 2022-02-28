38c40afb668218a1fbe9ddf237827205c573eafb
Philip Boardman <dustykeyboard@outlook.com.au>
2022-02-28 13:20:54 +1100

    Cleanup for eleventy generated site

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addWatchTarget("./src/styles.css");
-  eleventyConfig.addPassthroughCopy("./src/*/*.png");
-  eleventyConfig.addWatchTarget("./src/*/*.png");
-  eleventyConfig.addPassthroughCopy("./src/*.jpg");
-  eleventyConfig.addWatchTarget("./src/*.jpg");
+  eleventyConfig.addPassthroughCopy("src/**/*.{jpg,jpeg,png,gif}");
   return {
     dir: {
       input: "src",
