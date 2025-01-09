31d5fb73b4a39b5839e653f91d95922451ed1ea1
Philip Boardman <philip.boardman@redbubble.com>
2025-01-09 11:41:33 +1100

    Add email address

---
+++ src/js/email.js
@@ -0,0 +1,17 @@
+document.getElementById("email").href =
+  "mail" +
+  "to" +
+  ":" +
+  "jobs" +
+  "@" +
+  "brd" +
+  "." +
+  "mn" +
+  "?subject=" +
+  "brd" +
+  "." +
+  "mn" +
+  "%20" +
+  "email" +
+  "%20" +
+  "contact";
