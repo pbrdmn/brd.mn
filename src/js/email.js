585f0ce5a0639922aa82099b5e6821744b601310
Philip Boardman <philip.boardman@redbubble.com>
2025-01-10 11:35:05 +1100

    Fix email js error

--- src/js/email.js
+++ src/js/email.js
@@ -1,17 +1,20 @@
-document.getElementById("email").href =
-  "mail" +
-  "to" +
-  ":" +
-  "jobs" +
-  "@" +
-  "brd" +
-  "." +
-  "mn" +
-  "?subject=" +
-  "brd" +
-  "." +
-  "mn" +
-  "%20" +
-  "email" +
-  "%20" +
-  "contact";
+var e = document.getElementById("email");
+if (e?.href) {
+  e.href =
+    "mail" +
+    "to" +
+    ":" +
+    "jobs" +
+    "@" +
+    "brd" +
+    "." +
+    "mn" +
+    "?subject=" +
+    "brd" +
+    "." +
+    "mn" +
+    "%20" +
+    "email" +
+    "%20" +
+    "contact";
+}
